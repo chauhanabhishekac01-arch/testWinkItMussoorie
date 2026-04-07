0c0e013a37f60b4d4be86233e8a257285c4aece9
chauhanabhishekac01-arch <chauhan.abhishek.ac01@gmail.com>
2026-04-07 23:47:05 +0530

    Revise service worker for caching and installation
    Updated service worker to new version and changed cache name.

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-const CACHE_NAME = 'wink-it-v3'; // Increment this every time you change code!
+const GHPATH = '/testWinkItMussoorie';
+const CACHE_NAME = 'wink-it-v1';
 const ASSETS = [
   '/',
   '/index.html',
@@ -8,21 +9,20 @@ const ASSETS = [
   '/intro.mp4'
 ];
 
+// Install Service Worker
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
   );
 });
 
-// Add this: allows the new SW to take over immediately
-self.addEventListener('message', (event) => {
-  if (event.data === 'skipWaiting') {
-    self.skipWaiting();
-  }
-});
-
+// Fetch Assets
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => response || fetch(event.request))
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
   );
 });
